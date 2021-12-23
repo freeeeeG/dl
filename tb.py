65f70667f0fb589dc53a083267c7f611df5a1019
freeeeeG <1284566797@qq.com>
2021-12-23 19:21:27 +0800

    haoye

--- tb.py
+++ tb.py
@@ -1,20 +1,22 @@
 from torch.utils.tensorboard import SummaryWriter
 from MyData import * 
 import numpy as np
-
+from torchvision import transforms
 
 root_dir = "101_ObjectCategories"
 ants_label_dir = "ant"
 ants_dataset = MyData(root_dir,ants_label_dir)
-img ,label=ants_dataset[0]
-img_array = np.array(img)
+
 
 writer = SummaryWriter("logs")
-
+img2tensor = transforms.ToTensor()
 
 for i in range(40):
     try:
+        img ,label=ants_dataset[i]
+        img_array = np.array(img)
         writer.add_image("test",img_array,i,dataformats='HWC')
+        tensors_img = img2tensor(img)
     except:
         print(i)
 
