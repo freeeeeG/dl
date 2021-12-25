662bbe885f9932a900ee780fecf577dd6ee7ad0f
freeeeeG <1284566797@qq.com>
2021-12-25 12:04:11 +0800

    tourdataset

--- tb.py
+++ tb.py
@@ -1,9 +1,15 @@
 from numpy.core.fromnumeric import resize
+from numpy.core.numeric import False_
+from torch.utils.data import dataset
 from torch.utils.tensorboard import SummaryWriter
+import torchvision
 from torchvision.transforms.transforms import Normalize
 from MyData import * 
 import numpy as np
 from torchvision import transforms
+import ssl
+# 取消全局证书认证
+ssl._create_default_https_context = ssl._create_unverified_context
 
 root_dir = "101_ObjectCategories"
 ants_label_dir = "ant"
@@ -35,13 +41,30 @@ for i in range(40):
         trans_resize_2 = transforms.Resize(512)
         trans_compose = transforms.Compose([trans_resize_2,img2tensor])
         img_resize_2 = trans_compose(img)
-
+        # Randomcrop
+        trans_random = transforms.RandomCrop(512)
+        trans_compose2 = transforms.Compose([trans_random,img2tensor])
+        for i in range(10):
+            img_corp = trans_compose2(img)
+            writer.add_image("corp",img_corp,i)
     except:
         print(i)
 
 
 for i in range(100):
     writer.add_scalar("y=x",i,i)
+
+
+
+dataset_transform = transforms.Compose([torchvision.transforms.ToTensor()])
+train_set = torchvision.datasets.CIFAR10(root="./dataset", transform = dataset_transform, train=True, download=True)
+test_set = torchvision.datasets.CIFAR10(root="./dataset", transform = dataset_transform, train=False, download=True)
+
+writer = SummaryWriter("test")
+for i in range(10):
+    img,label = test_set[i]
+    writer.add_image("dataset", img, i)
+writer.close()
 
 
 writer.close()
