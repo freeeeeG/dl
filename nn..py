f4d8144daf25ea7f8c41113f739077ff9ed16741
freeeeeG <1284566797@qq.com>
2021-12-25 22:09:53 +0800

    nn_base

--- nn..py
+++ nn..py
@@ -12,8 +12,8 @@ class Model(nn.Module):
         F.relu(self.conv1(x))
         return F.relu(self.conv2(x))
         return x+1
-# torch.set_default_tensor_type(torch.DoubleTensor)
-test = torch.rand(5,5)
+# torch.set_default_tensor_type(torch.IntTensor)
+test = torch.randint(0,10,size=(5,5))
 # test = torch.tensor([[1,2,1,1,0],
 #                         [2,3,1,1,0],
 #                         [2,3,1,1,0],
@@ -26,4 +26,12 @@ kernel = torch.tensor([[1,2,1],
 test = torch.reshape(test,(1, 1, 5, 5))
 kernel = torch.reshape(kernel,(1, 1, 3, 3))
 
-print(out = F.conv2d(test,kernel,stride=1))
+
+print(F.conv2d(test, kernel, stride=1))
+
+print(F.conv2d(test, kernel, stride=2))
+
+
+
+
+
